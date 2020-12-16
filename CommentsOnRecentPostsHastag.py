6d0093c2aed0a1f38f5c941ea77528b993a14762
CriticalInfinity <daniyalanis98@gmail.com>
2020-12-16 17:53:31 +0530

    Added way to input comments

--- CommentsOnRecentPostsHastag.py
+++ CommentsOnRecentPostsHastag.py
@@ -10,4 +10,12 @@ print("Enter the hastags without space")
 for tags in range(numHastag):
     hastags.append(input("Enter hashtag "+str(x+1)+" : "))
 
-for 
+numComment=0
+while numComment<=0:
+    numComment=int(input("Enter the number of hastags you want to enter "))
+print("Enter the comments ")
+comments=[]
+for comment in range(numComment):
+    comment.append("Enter comment "+str(x+1)+" : ")
+
+com_per_hastag=int(input("Enter the number of comments you want to post per hastag : "))
