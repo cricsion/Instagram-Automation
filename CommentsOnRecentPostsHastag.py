7704830af319840fa2d6c289f828199387f149a9
CriticalInfinity <daniyalanis98@gmail.com>
2020-12-17 17:24:41 +0530

    Added function to open first recent post and solved some issues

--- CommentsOnRecentPostsHastag.py
+++ CommentsOnRecentPostsHastag.py
@@ -4,18 +4,28 @@ from time import sleep
 
 hastags=[]
 numHastag=0
-while numHastag<=0:
-    numHastag=int(input("Enter the number of hastags you want to enter "))
+while numHastag<=0: # To prevent users from inputting number of hastags as less than 0 or equal to 0
+    numHastag=int(input("Enter the number of hastags you want to enter : "))
 print("Enter the hastags without space")
-for tags in range(numHastag):
-    hastags.append(input("Enter hashtag "+str(x+1)+" : "))
+for tags in range(numHastag): #Takes input of hastags 
+    hastags.append(input("Enter hashtag "+str(tags+1)+" : "))
 
 numComment=0
-while numComment<=0:
-    numComment=int(input("Enter the number of hastags you want to enter "))
+while numComment<=0: #Same reason as in line 7
+    numComment=int(input("Enter the number of comments you want to enter : "))
 print("Enter the comments ")
 comments=[]
-for comment in range(numComment):
-    comment.append("Enter comment "+str(x+1)+" : ")
+for comment in range(numComment): #Takes input of the comments the user wants to comment on post
+    comments.append("Enter comment "+str(comment+1)+" : ")
 
-com_per_hastag=int(input("Enter the number of comments you want to post per hastag : "))
+def Opens_First_Recent_Post(): #Opens First Recent Post at the time
+    driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]/a/div/div[1]/img').click()
+    sleep(2)
+
+com_per_hastag=0
+while com_per_hastag<=0:
+    com_per_hastag=int(input("Enter the number of comments you want to post per hastag : "))
+
+for tag in hastags:
+    for com in range(com_per_hastag):
+        driver.get('https://www.instagram.com/explore/tags/{}/'.format(tag))
